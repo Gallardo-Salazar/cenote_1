882ae2dded050a58cb864907743ddb040cfdd98b
unknown <https://github.com/Gallardo-Salazar/socavon_tlanepantla.git>
2023-11-10 15:57:05 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,83 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 9747160,
+    "boundingBox": {
+        "lx": 486403.2872,
+        "ly": 2277119.7636,
+        "lz": -7.317,
+        "ux": 486521.38420000005,
+        "uy": 2277237.8606000004,
+        "uz": 110.78000000000884
+    },
+    "tightBoundingBox": {
+        "lx": 486403.2872,
+        "ly": 2277119.7636,
+        "lz": -7.317,
+        "ux": 486521.38420000005,
+        "uy": 2277203.6076,
+        "uz": 10.435
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.0227500200271607,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
